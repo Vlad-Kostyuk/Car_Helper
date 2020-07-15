b0f8002fd1534e5a21116627e798b6c13dd3baf8
Vlad-Kostyuk <vlad.kostyuk24@gmail.com>
2020-07-15 19:25:36 +0300

    add design add page

--- lib/widget/center_add_inspection.dart
+++ lib/widget/center_add_inspection.dart
@@ -1,4 +1,6 @@
+import 'package:date_field/date_field.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class BodyAddInspection extends StatefulWidget {
   const BodyAddInspection({Key key}) : super(key: key);
@@ -8,34 +10,103 @@ class BodyAddInspection extends StatefulWidget {
 }
 
 class _BodyAddInspectionState extends State<BodyAddInspection> {
+
+  DateTime selectedDate;
+
+  static DateTime now = DateTime.now();
+  static int thisYear = (int.parse(DateFormat.y().format(now)) + 1);
+  int firstYear = (thisYear - 5);
+
+
   @override
   Widget build(BuildContext context) {
+
     return Container(
       child: Column(
         children: <Widget>[
 
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Password',
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Expanded(
+              flex: 1,
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Name Inspection',
+                    ),
+                  ),
+
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Descripshon',
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
 
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Password',
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Expanded(
+              flex: 1,
+              child: Column(
+                children: <Widget>[
+
+                 DropdownButton<String>(
+
+                 ),
+
+                ],
+              ),
             ),
           ),
 
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Password',
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Expanded(
+              flex: 1,
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Km',
+                    ),
+                  ),
+
+                  Text(
+                    'or',
+                  ),
+
+                  DateField(
+                      lastDate: DateTime(thisYear),
+                      firstDate: DateTime(firstYear),
+                      selectedDate: now,
+                      onDateSelected: (DateTime date){
+                        setState(() {
+                          selectedDate = date;
+                        });
+                      }
+                  ),
+                ],
+              ),
             ),
+          ),
+
+          Center(
+            child: RaisedButton(
+              onPressed: null,
+              child: Text(
+                'add'
+              ),
+            )
           ),
 
         ],
